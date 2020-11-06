561bdb187bd27e24ca1337388a3f311d9d766bad
vagrant <avedj94@hotmail.com>
2020-11-06 04:42:51 +0000

    refactor using promise then

---
+++ iss_promised.js
@@ -0,0 +1,27 @@
+const request = require('request-promise-native');
+
+const fetchMyIP = function() {
+  return request("https://api.ipify.org/?format=json");
+}
+
+const fetchCoordsByIP = function(body) {
+  let ip = JSON.parse(body).ip;
+  return request("http://ip-api.com/json/" + ip);
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  let data = JSON.parse(body);
+  return request("http://api.open-notify.org/iss-pass.json?lat=" + data.lat + "&lon=" + data.lon);
+}
+
+const nextISSTimesForMyLocation = function(body) {
+  return fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then((data) => {
+    const {response} = JSON.parse(data);
+    return response;
+  });
+};
+
+module.exports = { nextISSTimesForMyLocation };
